51dbab8b1009c320efd1f1d9fc0d746e17ca889b
Manuel <visiont3lab@gmail.com>
2021-02-24 20:12:46 +0100

    update

--- part2.py
+++ part2.py
@@ -5,34 +5,39 @@ import os
 cap = cv2.VideoCapture(1)
 faceDetector = cv2.CascadeClassifier(os.path.join("haar-cascade-files","haarcascade_frontalface_default.xml"))
 smileDetector = cv2.CascadeClassifier(os.path.join("haar-cascade-files","haarcascade_smile.xml"))
-eyeDetector = cv2.CascadeClassifier(os.path.join("haar-cascade-files","haarcascade_eye.xml"))
+eyeDetector = cv2.CascadeClassifier(os.path.join("haar-cascade-files","haarcascade_eye_tree_eyeglasses.xml"))
 
 while(True):
     # Capture frame-by-frame
     try:
         ret, frame = cap.read()
         if ret:
-            # Face detector
-            #cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            #roi = frame[y:y+h,x:x+w]
-            
             # minNeighbors > siamo sicuri che sia una faccia per
             faces = faceDetector.detectMultiScale(frame,scaleFactor=1.5,minNeighbors=8,flags=cv2.CASCADE_DO_ROUGH_SEARCH | cv2.CASCADE_SCALE_IMAGE)
-           
-            for x,y,w,h in faces:
-                roi = frame[y:y+h,x:x+w]
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2) # BGR
+
+            for xf,yf,wf,hf in faces:
+                roi = frame[yf:yf+hf,xf:xf+wf]
+
+                roiBlur = cv2.medianBlur(roi, 15)
+                roiHSV = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+                frame[yf:yf+hf,xf:xf+wf] = roiBlur
+
+                cv2.rectangle(frame,(xf,yf),(xf+wf,yf+hf),(255,0,0),2) # BGR
 
                 eyes = eyeDetector.detectMultiScale(roi,scaleFactor=1.05,minNeighbors=3,flags=cv2.CASCADE_DO_ROUGH_SEARCH | cv2.CASCADE_SCALE_IMAGE)
-                for x,y,w,h in eyes:
-                    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2) # BGR
+                for xe,ye,we,he in eyes:
+                    xn = xf +xe
+                    yn = yf + ye
+                    cv2.rectangle(frame,(xn,yn),(xn+we,yn+he),(0,255,0),2) # BGR
 
                 smiles = smileDetector.detectMultiScale(roi,scaleFactor=1.5,minNeighbors=15,flags=cv2.CASCADE_DO_ROUGH_SEARCH | cv2.CASCADE_SCALE_IMAGE)
-                for x,y,w,h in smiles:
-                    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2) # BGR
+                for xs,ys,ws,hs in smiles:
+                    xn = xf +xs
+                    yn = yf + ys
+                    cv2.rectangle(frame,(xn,yn),(xn+ws,yn+hs),(0,0,255),2) # BGR
                     
-                #roi  = cv2.resize(roi, (412,412))
-                #cv2.imshow('roi',roi)
+                #roiBlur  = cv2.resize(roiBlur, (412,412))
+                #cv2.imshow('roi',roiBlur)
 
             frame = cv2.putText(frame,"Ciao", (50, 50), cv2.FONT_HERSHEY_SIMPLEX,  1, (255, 0, 0) , 2, cv2.LINE_AA) 
             
